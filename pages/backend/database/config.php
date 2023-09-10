d24b50959ad2cbf260a834cd8b066122dd72b0e4
Jesus Alberto Castillo Hernandez <jesusach92@gmail.com>
2023-09-10 00:00:28 -0600

    Cambios

---
+++ pages/backend/database/config.php
@@ -0,0 +1,7 @@
+<?php
+//se da la conexion
+$conexion = mysqli_connect("localhost:3306", "root", "R1n0c3r0nt3.T", "quinielas_compas");
+//                    nombre servidor--usuario--contraseña base de datos
+
+?>
+    
