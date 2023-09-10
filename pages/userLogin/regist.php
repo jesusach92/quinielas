80b4cf45e62858db2f9f8f0e04f5296c1adbf998
OctavioCass <danidanieloctavio@gmail.com>
2023-09-09 22:14:44 -0600

    redireccion despues del login

--- pages/userLogin/regist.php
+++ pages/userLogin/regist.php
@@ -1,7 +1,16 @@
 <?php
-include("config.php");
-    if ($conexion){
-        echo('bien');
+include("config.php"); // Asegúrate de que config.php configure correctamente la conexión a la base de datos
+
+if ($conexion) {
+    // La conexión a la base de datos se estableció correctamente
+    header("Location: ../admin/index.html");
+    
+    // Aquí puedes realizar otras acciones, como verificar si los datos del formulario están en la base de datos, etc.
+} else {
+    // Si la conexión a la base de datos falla, muestra un mensaje de error
+    echo('<script>alert("Error: No se pudo establecer la conexión a la base de datos.");</script>');
 }
-//verificacion si esta en la base de datos 
+
+// Luego, puedes incluir cualquier contenido adicional que desees mostrar en la página HTML
+//include("contenido_adicional.php");
 ?>
