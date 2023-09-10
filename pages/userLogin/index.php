28bf3dd9c9e29756719e69c6e193b95f72afc7c3
OctavioCass <danidanieloctavio@gmail.com>
2023-09-09 18:20:43 -0600

    php primer cambio con base sql-not tested

--- pages/userLogin/index.php
+++ pages/userLogin/index.php
@@ -1,9 +1,6 @@
 <?php
-// Definir usuarios y contraseñas válidos (puedes reemplazarlos por una base de datos real)
-$usuarios = array(
-    'usuario1' => 'contraseña1',
-    'usuario2' => 'contraseña2'
-);
+// Incluir archivo de configuración de la base de datos
+include_once 'config.php'; 
 
 // Inicializar variables para mensajes de error y redirección
 $mensaje = '';
@@ -15,24 +12,75 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $usuario = $_POST['username'];
     $contrasena = $_POST['password'];
 
-    // Verificar si el usuario y la contraseña son válidos
-    if (isset($usuarios[$usuario]) && $usuarios[$usuario] === $contrasena) {
-        //isset dice si la variable esta ocupada o null
-        // Inicio de sesión exitoso, configurar mensaje y redirección
-        $mensaje = 'Inicio de sesión exitoso.';
-        $redireccionar = true;
-    } else {
-        // Inicio de sesión fallido, configurar mensaje de error
-        $mensaje = 'Usuario o contraseña incorrectos.';
+    // Conectar a la base de datos
+    $conn = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+    // Verificar si la conexión es exitosa
+    if (!$conn) {
+        die("Conexión fallida: " . mysqli_connect_error());
     }
+
+    // Preparar la consulta SQL para buscar el usuario en la tabla
+    $sql = "SELECT * FROM usuarios WHERE username = '$usuario'";
+    $result = mysqli_query($conn, $sql);
+
+    if ($result) {
+        if (mysqli_num_rows($result) > 0) {
+            $row = mysqli_fetch_assoc($result);
+            // Verificar si la contraseña coincide
+            if (password_verify($contrasena, $row['password'])) {
+                // Inicio de sesión exitoso, configurar mensaje y redirección
+                $mensaje = 'Inicio de sesión exitoso.';
+                $redireccionar = true;
+            } else {
+                // Inicio de sesión fallido, configurar mensaje de error
+                $mensaje = 'Usuario o contraseña incorrectos.';
+            }
+        } else {
+            // Usuario no encontrado en la base de datos
+            $mensaje = 'Usuario o contraseña incorrectos.';
+        }
+    } else {
+        // Error en la consulta SQL
+        $mensaje = 'Error en la consulta SQL: ' . mysqli_error($conn);
+    }
+
+    // Cerrar la conexión a la base de datos
+    mysqli_close($conn);
 }
-//ahora la redireccion para la pagina de las quiniela
 
 // Si se debe redirigir al usuario, hacerlo
 if ($redireccionar) {
-    header("Location: PP.php"); // Reemplaza "otra_pagina.php" con la URL de la página a la que deseas redirigir
-    /*echo '<meta http-equiv="refresh" content="0;url=PP.php">'; otro metodo de redireccion */
-    /* echo '<script>window.location.href = "PP.php";</script>'; otro metodo de redireccion  */
-    exit;//detiene el codigo para que no se siga ejecutando despues de que se va a la pagina 
+    header("Location: index.html"); // Reemplaza "index.html" con la URL de la página a la que deseas redirigir
+    exit; // Detiene el código para que no se siga ejecutando después de la redirección
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Iniciar sesión</title>
+</head>
+<body>
+    <h1>Iniciar sesión</h1>
+
+    <?php
+    // Mostrar mensajes de error o éxito si existen
+    if (!empty($mensaje)) {
+        echo '<p>' . $mensaje . '</p>';
+    }
+    ?>
+
+    <form method="post" action="">
+        <label for="username">Usuario:</label>
+        <input type="text" id="username" name="username" required><br><br>
+
+        <label for="password">Contraseña:</label>
+        <input type="password" id="password" name="password" required><br><br>
+
+        <input type="submit" value="Iniciar sesión">
+    </form>
+</body>
+</html>
