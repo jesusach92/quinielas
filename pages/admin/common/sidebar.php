b2e76994fa1b39a43d9c14cf42e980b5ae1e40d8
OctavioCass <danidanieloctavio@gmail.com>
2023-09-23 13:34:14 -0600

    addcambios

--- pages/admin/common/sidebar.php
+++ pages/admin/common/sidebar.php
@@ -3,7 +3,7 @@
 <div class="rounded-t-lg">
     <ul class=" py-10 pl-8 pr-6 pt-10 flex flex-col gap-5 group ">
     <li class="hover:bg-slate-100"><a href='addQuiniela.php'> Agregar Quiniela</a> </li>
-    <li><a href='teams.php'>Cargar Equipos</a></li>
+    <li><a href='teams.html'>Cargar Equipos</a></li>
     <li class="hover:bg-slate-100"><a href='matches.php'> Consultar Quinielas</a> </li>
     <li>Cargar Resultados</li>
     <li>Borrar Quinielas</li>
