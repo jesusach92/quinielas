139d4d2de193f1488f30ed1a581404204003b5ec
Jesus Alberto Castillo Hernandez <jesusach92@gmail.com>
2023-09-27 22:26:48 -0600

    Add Match Fixed

--- controller/addMatch.php
+++ controller/addMatch.php
@@ -15,15 +15,17 @@ $resultadoJornadas = mysqli_query($conexion, $consultaJornadas);
 $valorPredeterminadoJornada = ""; // Puedes establecer el valor que desees como predeterminado
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["agregar_quiniela"])) {
+    
+    $numeroJornada = mysqli_real_escape_string($conexion, $_POST["numero_jornada"]);
     $equipoLocal = mysqli_real_escape_string($conexion, $_POST["equipo_local"]);
     $equipoVisitante = mysqli_real_escape_string($conexion, $_POST["equipo_visitante"]);
     $fechaPartido = mysqli_real_escape_string($conexion, $_POST["fecha_partido"]);
-    $numeroJornada = mysqli_real_escape_string($conexion, $_POST["numero_jornada"]);
+    
     $channel = mysqli_real_escape_string($conexion, $_POST["channel"]);
 
     // Verificar si los equipos seleccionados son diferentes
     if ($equipoLocal != $equipoVisitante) {
-        $stmt = mysqli_prepare($conexion, "INSERT INTO partidos (teamLocal,teamVisitor, match_date, journeys, channel) VALUES (?, ?, ?, ?,?)");
+        $stmt = mysqli_prepare($conexion, "INSERT INTO partidos (teamLocal,teamVisitor, match_date, fkJornada, channel) VALUES (?, ?, ?, ?,?)");
 
         if ($stmt) {
             mysqli_stmt_bind_param($stmt, "sssis", $equipoLocal, $equipoVisitante, $fechaPartido, $numeroJornada, $channel);
@@ -38,9 +40,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST["agregar_quiniela"])) {
             }    
             mysqli_stmt_close($stmt);
         } else {
+            header("Location: ../view/failPage.php ");
+                $_SESSION["nextPage"] = "./addMatch.php";
+                $_SESSION["message"] = "Error al guardar los datos: " . mysqli_error($conexion);
             $message = "Error al preparar la declaración.";
         }
     } else {
+        header("Location: ../view/failPage.php ");
+                $_SESSION["nextPage"] = "./addMatch.php";
+                $_SESSION["message"] = "Error al guardar los datos: " . mysqli_error($conexion);
         $message = "Error: El equipo local y el equipo visitante deben ser diferentes.";
     }
 }
