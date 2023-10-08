82036edef3dd15b1a3567b464d62dbc1e79e365b
OctavioCass <danidanieloctavio@gmail.com>
2023-10-07 19:59:10 -0600

    intento por guardar quiniela en BD

--- public/procesar.php
+++ public/procesar.php
@@ -1,7 +1,8 @@
 <?php
-if(!isset($_SESSION))
-session_start();
+if (!isset($_SESSION)) session_start();
 
+
+var_dump($_POST);
 // Incluye el archivo de configuración de la base de datos
 include "../model/backend/database/config.php";
 
@@ -12,69 +13,69 @@ if (isset($_GET["jornada"])) {
     // Convierte la jornada seleccionada a un número entero seguro
     $jornadaSeleccionada = intval($jornadaSeleccionada);
     
-    $consulta = "SELECT * FROM partidosview WHERE numero_jornada = $jornadaSeleccionada";
-    $resultados = mysqli_query($conexion, $consulta);
-    
-    if (!$resultados) {
-        die("Error al obtener los resultados: " . mysqli_error($conexion));
-    }
+    // Consulta para obtener los partidos de la jornada seleccionada
+    // Consulta para obtener los partidos de la jornada seleccionada
+$consultaPartidos = "SELECT * FROM partidosview WHERE numero_jornada = $jornadaSeleccionada";
+$resultados = mysqli_query($conexion, $consultaPartidos);
 
-    // Genera un ID de usuario único (puedes usar una función de hash para esto)
-    $idUser = uniqid();
+if (!$resultados) {
+    die("Error al obtener los resultados: " . mysqli_error($conexion));
+}
 
+
+    // Procesar el formulario si se ha enviado
     if ($_SERVER["REQUEST_METHOD"] === "POST") {
-        // Verifica que se haya ingresado un nombre de usuario y un número de teléfono
-        $names = $_POST["names"];
-        $phoneNumber = $_POST["phoneNumber"];
-        if (empty($names) || empty($phoneNumber)) {
-            echo "Por favor, ingresa tu nombre y número de teléfono.";
-        } else {
-            // Preparar una consulta SQL para la inserción de datos de usuario
-            $sqlInsertUser = "INSERT INTO usuarios (id, name, phoneNumber) VALUES (?, ?, ?)";
-            $stmtInsertUser = mysqli_prepare($conexion, $sqlInsertUser);
-
-            if (!$stmtInsertUser) {
-                die("Error al preparar la consulta de inserción de usuario: " . mysqli_error($conexion));
+        $nombre = $_POST["nombre"];
+        $telefono = $_POST["telefono"];
+        var_dump($nombre, $telefono);
+        
+        // Obtener otros datos del formulario según tu estructura
+        // $otroDato = $_POST["nombre_del_campo"];
+        
+        // Insertar datos del usuario en la tabla "usuarios"
+        $sqlInsertUsuario = "INSERT INTO quinielas (name, phoneNumber) VALUES (?, ?)";
+        $stmtInsertUsuario = mysqli_prepare($conexion, $sqlInsertUsuario);
+        mysqli_stmt_bind_param($stmtInsertUsuario, "ss", $nombre, $telefono);
+        mysqli_stmt_execute($stmtInsertUsuario);
+        
+        // Obtener el ID del usuario insertado
+        $userId = mysqli_insert_id($conexion);
+        
+        // Iterar sobre las selecciones del usuario y guardarlas en la tabla "quinielas"
+        foreach ($_POST["selecciones"] as $partidoId => $resultados) {
+            // Procesar $partidoId y $resultados según tu estructura
+            // $otroDatoPartido = $resultados["nombre_del_campo"];
+            
+            // Insertar datos en la tabla "quinielas"
+            $sqlInsertQuiniela = "INSERT INTO quinielas (name, fkJornada, idUser) VALUES (?, ?, ?, ?, ?)";
+            $stmtInsertQuiniela = mysqli_prepare($conexion, $sqlInsertQuiniela);
+            mysqli_stmt_bind_param($stmtInsertQuiniela, "siiis", $jornadaSeleccionada, $partidoId, $userId, $resultados);
+            mysqli_stmt_execute($stmtInsertQuiniela);
+            
+            // Obtener el ID de la quiniela insertada
+            $quinielaId = mysqli_insert_id($conexion);
+            
+            // Iterar sobre los resultados del partido y guardarlos en la tabla "resultquiniela"
+            foreach ($resultados as $resultado) {
+                // Insertar datos en la tabla "resultquiniela"
+                $sqlInsertResultado = "INSERT INTO resultquiniela (fkQuiniela, fkPartido, resultMatch) VALUES (?, ?, ?)";
+                $stmtInsertResultado = mysqli_prepare($conexion, $sqlInsertResultado);
+                mysqli_stmt_bind_param($stmtInsertResultado, "iis", $quinielaId, $partidoId, $resultado);
+                mysqli_stmt_execute($stmtInsertResultado);
             }
-
-            // Vincular parámetros a la consulta preparada
-            mysqli_stmt_bind_param($stmtInsertUser, "sss", $idUser, $names, $phoneNumber);
-
-            // Insertar los datos del usuario en la base de datos
-            if (mysqli_stmt_execute($stmtInsertUser)) {
-                echo "Usuario registrado con éxito.";
-            } else {
-                echo "Error al registrar el usuario: " . mysqli_error($conexion);
-            }
-
-            // Cerrar la consulta preparada
-            mysqli_stmt_close($stmtInsertUser);
-
-            // Preparar una consulta SQL para la inserción de selecciones de usuario
-            $sqlInsertSelections = "INSERT INTO quinielas (name, fkJornada, fkPartido, idUser, result) VALUES (?, ?, ?, ?, ?)";
-            $stmtInsertSelections = mysqli_prepare($conexion, $sqlInsertSelections);
-
-            if (!$stmtInsertSelections) {
-                die("Error al preparar la consulta de inserción de selecciones: " . mysqli_error($conexion));
-            }
-
-            // Vincular parámetros a la consulta preparada
-            mysqli_stmt_bind_param($stmtInsertSelections, "siiss", $names, $jornadaSeleccionada, $partidoId, $idUser, $resultado);
-
-            // Insertar las selecciones del usuario en la base de datos
-            foreach ($_POST["resultado"] as $partidoId => $resultado) {
-                $partidoId = intval($partidoId);
-                $resultado = mysqli_real_escape_string($conexion, $resultado);
-
-                if (mysqli_stmt_execute($stmtInsertSelections)) {
-                    echo "Selecciones guardadas con éxito.";
-                } else {
-                    echo "Error al guardar las selecciones: " . mysqli_error($conexion);
-                }
-            }
-
-            // Cerrar la consulta preparada
-            mysqli_stmt_close($stmtInsertSelections);
         }
-    }}
+        
+        // Cerrar las consultas preparadas
+        mysqli_stmt_close($stmtInsertUsuario);
+        mysqli_stmt_close($stmtInsertQuiniela);
+        mysqli_stmt_close($stmtInsertResultado);
+        
+         // Mostrar mensaje de éxito
+    echo "Tu quiniela se ha guardado correctamente.";
+
+    // Redirigir a la misma página después de unos segundos
+    header("refresh:5;url=./user.php");
+    exit(); // Importante: asegúrate de salir del script después de la redirección
+    }
+}
 ?>
--- public/user.php
+++ public/user.php
@@ -1,8 +1,39 @@
 <?php
-if(!isset($_SESSION))
-session_start();
+if (!isset($_SESSION)) session_start();
 require("./procesar.php");
+
+// Incluye el archivo de configuración de la base de datos
+include "../model/backend/database/config.php";
+
+// Verificar si se ha enviado el formulario
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    var_dump($_POST);
+    // Mostrar los datos del formulario
+    echo "<h2>Datos del Formulario:</h2>";
+    echo "Nombre: " . $_POST["nombre"] . "<br>";
+    echo "Teléfono: " . $_POST["telefono"] . "<br>";
+    echo "ID de Jornada: " . $_POST["jornadaId"] . "<br>";
+
+    echo "<h2>Selecciones del Usuario:</h2>";
+
+    // Decodificar las selecciones del usuario desde JSON a un array asociativo
+$userSelectionsData = json_decode($_POST["userSelectionsData"], true);
+
+// Verificar si la decodificación fue exitosa
+if ($userSelectionsData === null && json_last_error() !== JSON_ERROR_NONE) {
+    echo "Error al decodificar las selecciones del usuario.";
+} else {
+    // Iterar sobre las selecciones del usuario y mostrarlas
+    foreach ($userSelectionsData as $partidoId => $resultados) {
+        echo "Partido ID: " . $partidoId . "<br>";
+        echo "Resultados: " . implode(", ", $resultados) . "<br>";
+    }
+}
+}
+
+
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -11,15 +42,20 @@ require("./procesar.php");
     <title>Apuesta</title>
     <!-- Agrega los estilos de Tailwind CSS -->
     <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.15/dist/tailwind.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="./css/user.css">k
+    <link rel="stylesheet" href="./css/user.css">
 </head>
 <body>
     <!-- Importa el encabezado desde el archivo externo -->
     <?php include_once '../view/common/header.php'; ?>
 
     <div class="container mx-auto mt-10">
-        <form id="apuestaForm" method="post" action="./procesar.php">
+        <form id="apuestaForm" method="post" action="procesar.php">
             <!-- Campo para que el usuario agregue su nombre -->
+             <!-- Campo oculto para almacenar el id de la jornada seleccionada -->
+             <input type="hidden" name="jornadaId" value="<?= isset($_GET["jornada"]) ? $_GET["jornada"] : "" ?>">
+             <input type="hidden" name="userSelectionsData" value="<?= json_encode($userSelections) ?>">
+
+
             <div class="mb-4">
                 <label for="nombre" class="block text-gray-700 font-bold mb-2">Nombre:</label>
                 <input type="text" id="nombre" name="nombre" required class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline">
@@ -42,32 +78,55 @@ require("./procesar.php");
                     </tr>
                 </thead>
                 <tbody>
-                    <?php
-                    // Ejemplo de consulta a la base de datos utilizando mysqli_fetch_assoc
-                    while ($partido = mysqli_fetch_assoc($resultados)) {
-                        $partidoId = $partido["id"];
-                    ?>
-                    <tr>
-                        <td>
-                            <div class="circle-green hover:bg-green-700 text-xl" onclick="toggleResult(this, <?= $partidoId ?>, 'local')">L</div>
-                        </td>
-                        <td>
-                            <p class="text-lg"><?= $partido["nombreLocal"] ?></p>
-                            <img src="<?= $partido['banderaLocal'] ?>" alt="Bandera Local" style="width: 20%; height: auto; margin: 0 auto; display: block;">
-                        </td>
-                        <td>
-                            <div class="circle-green hover:bg-green-700 text-xl" onclick="toggleResult(this, <?= $partidoId ?>, 'empate')">E</div>
-                        </td>
-                        <td>
-                            <p class="text-lg"><?= $partido["nombreVisitor"] ?></p>
-                            <img src="<?= $partido['banderaVisitor'] ?>" alt="Bandera Visitante" style="width: 20%; height: auto; margin: 0 auto; display: block;">
-                        </td>
-                        <td>
-                            <div class="circle-green hover:bg-green-700 text-xl" onclick="toggleResult(this, <?= $partidoId ?>, 'visitante')">V</div>
-                        </td>
-                    </tr>
-                    <?php } // Fin del bucle while ?>
-                </tbody>
+                    
+                <?php
+// Muestra el contenido de $resultados
+var_dump($resultados);
+
+// Obtén la selección del usuario del array $resultados
+$seleccionUsuario = isset($resultados[0]) ? $resultados[0] : "";
+
+foreach ($partidos as $partido) :
+    $partidoId = $partido["id"];
+    $nombreLocal = $partido["nombreLocal"];
+    $banderaLocal = $partido["banderaLocal"];
+    $nombreVisitor = $partido["nombreVisitor"];
+    $banderaVisitor = $partido["banderaVisitor"];
+    
+    // Verificar si el partidoId está presente en las selecciones del usuario
+    $seleccionesUsuario = isset($userSelections[$partidoId]) ? $userSelections[$partidoId] : [];
+
+    // Comparar con la selección del usuario obtenida de $resultados
+    $seleccionLocal = ($seleccionUsuario === 'local');
+    $seleccionEmpate = ($seleccionUsuario === 'Empate');
+    $seleccionVisitante = ($seleccionUsuario === 'visitante');
+?>
+
+<tr>
+    <td>
+        <div class="circle-green <?= $seleccionLocal ? 'circle-red' : '' ?> hover:bg-green-700 text-xl" onclick="toggleResult(this, <?= $partidoId ?>, 'local')">L</div>
+    </td>
+    <td>
+        <p class="text-lg"><?= $nombreLocal ?></p>
+        <img src="<?= $banderaLocal ?>" alt="Bandera Local" style="width: 20%; height: auto; margin: 0 auto; display: block;">
+    </td>
+    <td>
+        <div class="circle-green <?= $seleccionEmpate ? 'circle-red' : '' ?> hover:bg-green-700 text-xl" onclick="toggleResult(this, <?= $partidoId ?>, 'Empate')">E</div>
+    </td>
+    <td>
+        <p class="text-lg"><?= $nombreVisitor ?></p>
+        <img src="<?= $banderaVisitor ?>" alt="Bandera Visitante" style="width: 20%; height: auto; margin: 0 auto; display: block;">
+    </td>
+    <td>
+        <div class="circle-green <?= $seleccionVisitante ? 'circle-red' : '' ?> hover:bg-green-700 text-xl" onclick="toggleResult(this, <?= $partidoId ?>, 'visitante')">V</div>
+    </td>
+</tr>
+
+<?php endforeach; ?>
+
+    
+</tbody>
+
             </table>
             <div id="totalPrice">Costo total: $ 0.00</div>
             <input type="submit" value="Enviar Apuesta" class="mt-4 bg-green-700 hover:bg-green-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full sm:w-auto">
@@ -104,34 +163,43 @@ require("./procesar.php");
             calculateTotalPrice();
         }
 
-        // Función para calcular el precio total
- function calculateTotalPrice() {
-    const partidoIds = Object.keys(userSelections);
-    let totalPrice = 10;
-    partidoIds.forEach(partidoId => {
-        const selections = userSelections[partidoId];
-        if (selections.length === 3) 
-            totalPrice = (totalPrice * 2) * 2.5;
-        if(selections.length === 2)
-         totalPrice = totalPrice *2 ;
-    });
-    document.getElementById('totalPrice').textContent = `Costo total: $${totalPrice}.00`;
-}
-        // Agrega una función para enviar el formulario con las selecciones del usuario
-        document.getElementById('apuestaForm').addEventListener('submit', function (event) {
-            // Agrega las selecciones del usuario como un campo oculto en el formulario
-            const input = document.createElement('input');
-            input.type = 'hidden';
-            input.name = 'selecciones';
-            input.value = JSON.stringify(userSelections);
-            this.appendChild(input);
-
-            const total = document.createElement('input');
-            total.type = 'hidden';
-            total.name = 'totalPrice';
-            total.value = JSON.stringify(totalPrice);
-            this.appendChild(total);
+       
+    // Función para calcular el precio total
+    function calculateTotalPrice() {
+        const partidoIds = Object.keys(userSelections);
+        let totalPrice = 10;
+        partidoIds.forEach(partidoId => {
+            const selections = userSelections[partidoId];
+            if (selections.length === 3) 
+                totalPrice = (totalPrice * 2) * 2.5;
+            if(selections.length === 2)
+                totalPrice = totalPrice * 2;
         });
-    </script>
+        document.getElementById('totalPrice').textContent = `Costo total: $${totalPrice}.00`;
+
+        // Actualiza el valor del campo oculto con el precio total
+        document.getElementById('totalPriceInput').value = totalPrice;
+    }
+
+    // Agrega una función para enviar el formulario con las selecciones del usuario
+    document.getElementById('apuestaForm').addEventListener('submit', function (event) {
+        // Agrega las selecciones del usuario como un campo oculto en el formulario
+        const input = document.createElement('input');
+        input.type = 'hidden';
+        input.name = 'userSelectionsData'; // Cambiado el nombre del campo
+        input.value = JSON.stringify(userSelections);
+        this.appendChild(input);
+
+        // Crea un campo oculto para enviar el precio total
+        const total = document.createElement('input');
+        total.type = 'hidden';
+        total.name = 'totalPrice';
+        total.id = 'totalPriceInput'; // Asigna un ID para poder actualizar su valor fácilmente
+        total.value = ''; // Deja el valor en blanco por ahora
+        this.appendChild(total);
+    });
+
+    
+</script>
 </body>
 </html>
